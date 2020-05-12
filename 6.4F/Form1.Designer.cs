66c999bae8f091e890693e278b4d62f29c93da76
Миша Епринцев <meprincev@bk.ru>
2020-05-12 23:48:31 +0300

    123

---
+++ 6.4F/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _6._4F
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.N = new System.Windows.Forms.TextBox();
+            this.V = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Жми";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.Client;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите N";
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(12, 36);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(100, 20);
+            this.N.TabIndex = 2;
+            // 
+            // V
+            // 
+            this.V.Location = new System.Drawing.Point(139, 9);
+            this.V.Multiline = true;
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(211, 133);
+            this.V.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.V);
+            this.Controls.Add(this.N);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox N;
+        private System.Windows.Forms.TextBox V;
+    }
+}
+
---
+++ 6.4F/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6._4F
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int[][] Input()
+        {
+            Random rnd = new Random();
+            int n = int.Parse(N.Text);
+            int[][] a = new int[n][];
+            for (int i = 0; i < n; ++i)
+            {
+                a[i] = new int[n];
+                for (int j = 0; j < n; ++j)
+                {
+                    a[i][j] = rnd.Next(-2, 2);
+                }
+            }
+            return a;
+        }
+
+        private void Print1(int[] a)
+        {
+            V.Text += Environment.NewLine;
+            for (int i = 0; i < a.Length; ++i)
+                V.Text += a[i] + "  ";
+        }
+
+        private void Print2(int[][] a)
+        {
+            V.Text += Environment.NewLine;
+            for (int i = 0; i < a.Length; ++i, V.Text += Environment.NewLine)
+                for (int j = 0; j < a[i].Length; ++j)
+                    V.Text +=  a[i][j] + "  ";
+        }
+
+        private int Otric(int[] a)
+        {
+            int j= -5;
+            for (int i = 0; i < a.Length; ++i)
+                
+                if (a[i] < 0) {j = i; break; }
+            return j;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                V.Text = "";
+                int[][] mas = Input();
+                V.Text += "Исходный массив:";
+                V.Text += Environment.NewLine;
+                Print2(mas);
+                int[] rez = new int[mas.Length];
+                for (int i = 0; i < mas.Length; ++i)
+                    rez[i] = Otric(mas[i]);
+                V.Text += Environment.NewLine;
+                V.Text += "Новый массив: ";
+                Print1(rez);
+            }
+            catch
+            {
+                V.Text += "Неккоректные данные";
+            }
+        }
+    }
+}
